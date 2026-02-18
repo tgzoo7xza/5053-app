fc5d2182c69c0a7dc63d7a4d5db5e1b65c41f1d4
tgzoo7xza <tgzoo7xza@gmail.com>
2026-02-18 14:40:41 +0700

    update1

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="th"> {""}
+    <html lang="th"> 
       <body
         // 3. เพิ่ม promptFont.className เข้าไปใน body
         className={`${promptFont.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
